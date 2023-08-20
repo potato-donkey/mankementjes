8a788696133e0f397f94b80ae9fe5bf56e67bbbf
jxpsert <j.platenburg@student.han.nl>
2023-08-20 15:12:04 +0200

    improved backend

--- api/database.js
+++ api/database.js
@@ -1,13 +1,18 @@
 const sqlite3 = require("sqlite3").verbose();
 
-let connection = new sqlite3.Database("./db/mankementjes.db", (err) => {
+const settings = require("./settings.json");
+
+const moment = require("moment");
+
+let connection = new sqlite3.Database(settings.dbpath, (err) => {
   if (err) return console.error(err.message);
 
   console.log("Connected to file database");
 });
 
-// Create mankementje table
-connection.run(`
+const setup = () => {
+  // Create mankementje table
+  connection.run(`
 CREATE TABLE IF NOT EXISTS mankementje (
     id INTEGER PRIMARY KEY,
     username TEXT,
@@ -23,8 +28,8 @@ CREATE TABLE IF NOT EXISTS mankementje (
 );
 `);
 
-// Create comment table
-connection.run(`
+  // Create comment table
+  connection.run(`
 CREATE TABLE IF NOT EXISTS comment (
     id INTEGER PRIMARY KEY,
     mankementje INTEGER,
@@ -35,15 +40,15 @@ CREATE TABLE IF NOT EXISTS comment (
 );
 `);
 
-// Create park table
-connection.run(`
+  // Create park table
+  connection.run(`
     CREATE TABLE IF NOT EXISTS park (
         park TEXT PRIMARY KEY
     );
 `);
 
-// Create location table
-connection.run(`
+  // Create location table
+  connection.run(`
     CREATE TABLE IF NOT EXISTS location (
         location TEXT,
         park TEXT,
@@ -52,8 +57,8 @@ connection.run(`
     );
 `);
 
-// Create section table
-connection.run(`
+  // Create section table
+  connection.run(`
     CREATE TABLE IF NOT EXISTS section (
         section TEXT,
         location TEXT,
@@ -62,6 +67,131 @@ connection.run(`
         PRIMARY KEY (location, park, section)
     );
 `);
+};
+
+const getAllMankementjes = (res) => {
+  connection.all(`SELECT * FROM mankementje`, [], (err, rows) => {
+    if (err) throw err;
+
+    const mankementjes = [];
+    let counter = 0;
+
+    rows.forEach((row) => {
+      connection.all(
+        `SELECT * FROM comment WHERE mankementje = ?`,
+        [row.id],
+        (err, comments) => {
+          if (err) throw err;
+          row.comments = [];
+          row.date = moment(row.date).format(settings.dateformat);
+
+          comments.forEach((comment) => {
+            row.comments.push(comment);
+          });
+
+          mankementjes.push(row);
+          counter++;
+          if (counter == rows.length) {
+            res.send(mankementjes);
+          }
+        }
+      );
+    });
+  });
+};
+
+const getMankementje = (id, res) => {
+  connection.get(`SELECT * FROM mankementje WHERE id = ?`, [id], (err, row) => {
+    if (err) throw err;
+
+    connection.all(
+      `SELECT * FROM comment WHERE mankementje = ?`,
+      [id],
+      (err, comments) => {
+        if (err) throw err;
+        row.comments = [];
+
+        row.date = moment(row.date).format(settings.dateformat);
+
+        let counter = 0;
+
+        comments.forEach((comment) => {
+          comment.date = moment(comment.date).format(settings.dateformat);
+
+          row.comments.push(comment);
+          counter++;
+          if (counter == comments.length) {
+            res.send(row);
+          }
+        });
+
+        if (comments.length == 0) {
+          res.send(row);
+        }
+      }
+    );
+  });
+};
+
+const getParks = (res) => {
+  connection.all(`SELECT * FROM park`, [], (err, rows) => {
+    if (err) throw err;
+    res.send(rows.map((row) => row.park)); // Send only park names
+  });
+};
+
+const getLocations = (park, res) => {
+  connection.all(
+    `SELECT * FROM location WHERE park = ?`,
+    [park],
+    (err, rows) => {
+      if (err) throw err;
+      const locations = rows.map((row) => row.location); // Send only location names
+      res.send({
+        park: park,
+        locations: locations,
+      });
+    }
+  );
+};
+
+const getSections = (park, location, res) => {
+  connection.all(
+    `SELECT * FROM section WHERE park = ? AND location = ?`,
+    [park, location],
+    (err, rows) => {
+      if (err) throw err;
+      const sections = rows.map((row) => row.section); // Send only section names
+      res.send({
+        park: park,
+        location: location,
+        sections: sections,
+      });
+    }
+  );
+};
+
+const getMankementjesInSection = (park, location, section, res) => {
+  connection.all(
+    `SELECT * FROM mankementje WHERE park = ? AND location = ? AND section = ?`,
+    [park, location, section],
+    (err, rows) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+};
+
+module.exports = {
+  connection,
+  setup,
+  getAllMankementjes,
+  getMankementje,
+  getParks,
+  getLocations,
+  getSections,
+  getMankementjesInSection,
+};
 
 // Filling db for testing
 
@@ -104,5 +234,3 @@ connection.run(`
 // connection.run(`INSERT INTO mankementje VALUES (1, 'japser', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Joris_en_de_Draak_-_Dragon.JPG/1280px-Joris_en_de_Draak_-_Dragon.JPG', 'Lampje kapot', 'Lampje kapot bij de colamachine', 'open', 'Efteling', 'Joris en de Draak', 'Wachtrij', '2023-08-18', null);`)
 
 // connection.run(`INSERT INTO comment VALUES (1,1, 'japser', 'Dit lampje is weer gemaakt!', '2023-08-18', 'public');`)
-
-module.exports = connection;
