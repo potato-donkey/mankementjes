29f3ea0b9fbb797469911a15026d13b9db0d8313
jxpsert <jplatenburg@outlook.com>
2024-01-29 01:05:57 +0100

    Solved merge stuffs

--- mankementjes/resources/views/mankementje.blade.php
+++ mankementjes/resources/views/mankementje.blade.php
@@ -43,78 +43,25 @@
     </div>
     <div class="row">
         <div class="col-12 col-md-8">
-<<<<<<< HEAD
             <img class="img of-c op-c w-100 mh-60 rounded mb-2 bg-black" src="{{ $mankementje['image'] }}">
             <h2>{{ $mankementje['location'] }} - {{ $mankementje['title'] }}</h2>
             <span class="mankementje-datum">Gemeld op {{ $mankementje['date'] }} door
-                @if ($user) {{ $user["name"] }} @else onbekend @endif &centerdot; {{$mankementje["status"]}}</span>
+                @if ($user)
+                    {{ $user['name'] }}
+                @else
+                    onbekend
+                @endif &centerdot; {{ $mankementje['status'] }}
+            </span>
             <p>
                 {{ $mankementje['description'] }}
             </p>
         </div>
         <div class="col-12 col-md-4 ps-md-5">
-            @unless( $mankementje["status"] == 'Opgelost' )
-                <a class='btn btn-success'><i class='bi bi-check'></i>&nbsp;Dit is opgelost!</a>
-                <a class='btn btn-danger' href="mailto:report@mankementjes.nl?subject=Report%3A%20mankementje%20%23{{ $mankementje['id'] }}"><i class='bi bi-exclamation-triangle-fill'></i>&nbsp;Rapporteer</a>
-=======
-            <img class="img w-100 of-c op-c mh-40 bg-primary rounded mb-2" src="{{ $mankementje['image'] }}">
-            <h2>
-                @if ($location)
-                    {{ $location['location'] }}
-                @else
-                    {{ 'Algemeen' }}
-                @endif - {{ $mankementje['title'] }}
-            </h2>
-            <span class="mankementje-datum">Gemeld op
-                {{ \App\Http\Controllers\DateController::renderFullDate($mankementje['date']) }} door
-                @if ($user)
-                    {{ $user['name'] }}
-                @else
-                    {{ '(onbekend)' }}
-                @endif &centerdot;
-
-                @php
-                    $statusText = $mankementje['status'];
-                    $statusText = strtolower($statusText);
-
-                    $colour = '';
-
-                    switch ($statusText) {
-                        case 'open':
-                            $colour = 'red';
-                            break;
-                        case 'opgelost':
-                            $colour = 'green';
-                            break;
-                        default:
-                            $colour = 'orange';
-                            break;
-                    }
-                @endphp
-                <span class="mankementje-status {{ $colour }}"
-                    title="{{ $status['description'] }}">{{ $status['status'] }}</span>
-                @if ($status['status'] == 'Opgelost' && !empty($mankementje['solve_date']))
-                    op {{ \App\Http\Controllers\DateController::renderFullDate($mankementje['solve_date']) }}
-                @endif
-            </span>
-            {!! str_replace(
-                "\n",
-                '<br />',
-                (new CommonMarkConverter($markdownConfig))->convert($mankementje['description'])->getContent(),
-            ) !!}
-        </div>
-        <div class="col-12 col-md-4 ps-md-5">
             @unless ($mankementje['status'] == 'Opgelost')
-                @auth
-                    @if ($mankementje['user_id'] == Auth::user()->id || Auth::user()->id == 0)
-                        <a class='btn btn-success' href="/mankementje/{{ $mankementje['id'] }}/solve"><i
-                                class='bi bi-check'></i>&nbsp;Dit is opgelost!</a>
-                    @endif
-                @endauth
+                <a class='btn btn-success'><i class='bi bi-check'></i>&nbsp;Dit is opgelost!</a>
                 <a class='btn btn-danger'
-                    href="mailto:report@mankementjes.nl?subject=Rapport%3A%20mankementje%20%23{{ $mankementje['id'] }}"><i
+                    href="mailto:report@mankementjes.nl?subject=Report%3A%20mankementje%20%23{{ $mankementje['id'] }}"><i
                         class='bi bi-exclamation-triangle-fill'></i>&nbsp;Rapporteer</a>
->>>>>>> 229b4d1c796af044ae696c31c0b1377c8eafb09e
             @endunless
 
             @php
@@ -125,8 +72,7 @@
             <h2 class="mt-3">Reacties</h2>
             <x-comment-list :comments="$comments" />
 
-<<<<<<< HEAD
-            @unless ($mankementje["status"] == 'Opgelost')
+            @unless ($mankementje['status'] == 'Opgelost')
                 <h3 class="mt-3">Reageer</h3>
                 <form action="/mankementje/{{ $mankementje['id'] }}/comment" method="POST">
                     @csrf
@@ -134,24 +80,9 @@
                         <textarea class="form-control" id="comment" name="comment" placeholder="Reactie" rows="3"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Verstuur</button>
-            @else
-                <span class="mankementje-datum mt-3">Dit mankementje is opgelost. Reageren is niet meer mogelijk.</span>
-            @endunless
-=======
-            @auth
-                @unless ($mankementje['status'] == 'Opgelost')
-                    <h3 class="mt-3">Reageer</h3>
-                    <form action="/mankementje/{{ $mankementje['id'] }}/comment" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <textarea class="form-control" id="comment" name="comment" rows="3" placeholder="Reactie"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Plaatsen</button>
-                    @else
-                        <span class="mankementje-datum mt-3">Dit mankementje is opgelost. Reageren is niet meer mogelijk.</span>
-                    @endunless
-                @endauth
->>>>>>> 229b4d1c796af044ae696c31c0b1377c8eafb09e
+                @else
+                    <span class="mankementje-datum mt-3">Dit mankementje is opgelost. Reageren is niet meer mogelijk.</span>
+                @endunless
 
                 @guest
                     <span class="mt-3"><a href="/me/login">Log in</a> om te reageren.</span>
